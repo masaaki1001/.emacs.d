5523dfc49ce06258c87030e904a36eff1e6e774e
masaaki <masa1001@gmail.com>
2012-04-07 10:36:09 +0900

    init-theme.el

--- init.el
+++ init.el
@@ -127,25 +127,7 @@
 ;;----------------------------------------------------------------------------
 ;; color-theme
 ;;----------------------------------------------------------------------------
-(when (require 'color-theme nil t)
-  (color-theme-initialize)
-  (require 'color-theme-solarized nil t)
-  (require 'color-theme-sanityinc-tomorrow nil t)
-  (require 'color-theme-tangotango nil t)
-  (require 'zenburn nil t)
-  )
-
-;;(require 'color-theme-railscasts)
-;;(color-theme-railscasts)
-;;(color-theme-subtle-blue)
-;;(color-theme-Gnome2)
-
-;; 取得元
-;; https://github.com/sellout/emacs-color-theme-solarized
-;; https://github.com/purcell/color-theme-sanityinc-tomorrow
-;; https://github.com/olegshaldybin/color-theme-railscasts
-;; https://github.com/juba/color-theme-tangotango
-;; https://github.com/credmp/color-theme-zenburn
+(require 'init-theme nil t)
 
 ;;----------------------------------------------------------------------------
 ;; Power Line for Emacs
