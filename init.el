a941b78ed3ca093e6315292732fd03a37399cc77
masaaki1001 <masa1001@gmail.com>
2012-07-20 10:29:14 +0900

    elpa

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
 ;;----------------------------------------------------------------------------
 (when (require 'package nil t)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  ;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
   (package-initialize)
 
