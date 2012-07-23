6de90e522acd9198c09b88dc6819a8fa44674244
masaaki1001 <masa1001@gmail.com>
2012-07-23 12:44:27 +0900

    add init-melpa.el

---
+++ config/init-elpa.el
@@ -0,0 +1,29 @@
+; -*- mode: lisp; coding: utf-8 -*-
+;;----------------------------------------------------------------------------
+;; package.el(marmalade) Emacs24からは標準搭載
+;; http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el
+;;----------------------------------------------------------------------------
+(when (require 'package nil t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (package-initialize)
+
+  ;; marmalade.el
+  ;; http://sheephead.homelinux.org/2011/06/17/6724/
+  (load "~/.emacs.d/elpa/marmalade-0.0.4/marmalade.el")
+  )
+
+
+;; https://github.com/purcell/emacs.d/blob/master/init-elpa.el
+(defun require-package (package &optional min-version no-refresh)
+  "Ask elpa to install given PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(provide 'init-elpa)
