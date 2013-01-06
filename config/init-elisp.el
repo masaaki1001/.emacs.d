b54f52f3ce245693503918d220fc4d3e66964834
masaaki1001 <masaaki@watanabemasaaki-no-MacBook-Pro.local>
2013-01-07 01:35:33 +0900

    update init-elips and submodule

--- config/init-elisp.el
+++ config/init-elisp.el
@@ -552,7 +552,7 @@
   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  ;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   )
 
 (provide 'init-elisp)
