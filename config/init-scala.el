85d9bc5635ece12d5ac089fe594a061466f040ee
masaaki1001 <masaaki@watanabemasaaki-no-MacBook-Pro.local>
2013-02-09 22:18:29 +0900

    ensime-setting

--- config/init-scala.el
+++ config/init-scala.el
@@ -3,6 +3,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/ensime_2.9.2-0.9.8.1/elisp")
 (when (require 'ensime nil t)
+  ;; ensime 同梱の auto-complete を使おうとするのを止める
+  (setq ensime-ac-override-settings nil)
   (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
   )
 
