d965c3b8dec148bb30be9a0a3614ac9265452f86
masaaki1001 <masa1001@gmail.com>
2013-03-03 10:35:13 +0900

    move twittering-mode

--- init.el
+++ init.el
@@ -88,12 +88,7 @@
 ;; http://www.emacswiki.org/emacs/TwitteringMode-ja
 ;; http://www.kototone.jp/com/how_to_use_twittering-mode.html
 ;;----------------------------------------------------------------------------
-(when (require 'twittering-mode nil t)
-  (setq twittering-use-master-password t)
-  (setq twittering-private-info-file "~/.emacs.d/resource/twittering-mode.gpg")
-  (setq twittering-icon-mode t)
-  ;; (setq twittering-status-format "%i @%s %S %p: \n %T  [%@]%r %R %f%L\n -------------------------------------------")
-  )
+(require 'init-twitter nil t)
 
 ;;----------------------------------------------------------------------------
 ;; migemo.el
