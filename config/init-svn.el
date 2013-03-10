6eebbaeef24e958b32fb60b67916091b0a5bb7be
masaaki1001 <masa1001@gmail.com>
2013-03-10 15:23:47 +0900

    init-svn

---
+++ config/init-svn.el
@@ -0,0 +1,34 @@
+;; dsvn.el
+;; Subversion用の設定 (psvn.elとdsvn.elを併用)
+;; http://d.hatena.ne.jp/hamaco/20090218/1234962837
+;; http://www23.atwiki.jp/selflearn/pages/41.html
+;; (require 'psvn)
+(autoload 'svn-status "dsvn" "Run `svn status'." t)
+(autoload 'svn-update "dsvn" "Run `svn update'." t)
+(setq process-coding-system-alist '(("svn" . utf-8)))
+(setq default-file-name-coding-system 'utf-8)
+(setq svn-status-svn-file-coding-system 'utf-8)
+(setq svn-status-svn-process-coding-system 'utf-8)
+(setq exec-path (cons "/usr/local/bin/" exec-path))
+;; http://d.hatena.ne.jp/yuto_sasaki/20120116/1326708562
+(setenv "LC_ALL" "ja_JP.UTF-8") ;; svn log が文字化けする対策
+
+(add-hook 'diff-mode-hook
+          (lambda ()
+            (set-face-foreground 'diff-context-face "grey50")
+            (set-face-background 'diff-header-face "black")
+            (set-face-underline-p 'diff-header-face t)
+            (set-face-foreground 'diff-file-header-face "MediumSeaGreen")
+            (set-face-background 'diff-file-header-face "black")
+            (set-face-foreground 'diff-index-face "MediumSeaGreen")
+            (set-face-background 'diff-index-face "black")
+            (set-face-foreground 'diff-hunk-header-face "plum")
+            (set-face-background 'diff-hunk-header-face"black")
+            (set-face-foreground 'diff-removed-face "pink")
+            (set-face-background 'diff-removed-face "gray5")
+            (set-face-foreground 'diff-added-face "light green")
+            (set-face-foreground 'diff-added-face "white")
+            (set-face-background 'diff-added-face "SaddleBrown")
+            (set-face-foreground 'diff-changed-face "DeepSkyBlue1")))
+
+(provide 'init-svn)
