b6f942d628bf5d8782951df439de81958630cbc9
masaaki1001 <masa1001@gmail.com>
2013-03-16 09:44:38 +0900

    add init-grep

---
+++ config/init-grep.el
@@ -0,0 +1,77 @@
+;; color-moccur.el
+;; http://d.hatena.ne.jp/IMAKADO/20080724/1216882563
+;; anythingとの連携あり
+;; http://www.bookshelf.jp/soft/meadow_50.html#SEC746
+;; http://www.bookshelf.jp/elc/color-moccur.el
+(when (require 'color-moccur nil t)
+  (setq moccur-split-word t)
+  ;; http://fkmn.exblog.jp/7311776/
+  (setq dmoccur-exclusion-mask
+        (append '("\\~$" "\\.svn\\/\*") dmoccur-exclusion-mask))
+  (setq dmoccur-exclusion-mask
+        (append '("\\~$" "\\.jpg\\/\*") dmoccur-exclusion-mask))
+  (setq dmoccur-exclusion-mask
+        (append '("\\~$" "\\.gif\\/\*") dmoccur-exclusion-mask))
+  (setq dmoccur-exclusion-mask
+        (append '("\\~$" "\\.tsv\\/\*") dmoccur-exclusion-mask))
+  (setq dmoccur-exclusion-mask
+        (append '("\\~$" "\\.log\\/\*") dmoccur-exclusion-mask))
+  )
+
+;; moccur-edit.el
+;; http://www.bookshelf.jp/elc/moccur-edit.el
+(when (require 'moccur-edit nil t))
+
+;; all-ext.el
+(when (require 'all-ext nil t))
+
+;; grep-a-lot.el
+;; Emacs tech book p162
+;; https://github.com/ZungBang/emacs-grep-a-lot
+(when (require 'grep-a-lot nil t)
+  (defvar my-grep-a-lot-search-word nil)
+  ;;上書き
+  (defun grep-a-lot-buffer-name (position)
+    "Return name of grep-a-lot buffer at POSITION."
+    (concat "*grep*<" my-grep-a-lot-search-word ">"))
+
+  (defadvice rgrep (before my-rgrep (regexp &optional files dir) activate)
+    (setq my-grep-a-lot-search-word regexp))
+
+  (defadvice lgrep (before my-lgrep (regexp &optional files dir) activate)
+    (setq my-grep-a-lot-search-word regexp))
+
+  ;; http://d.hatena.ne.jp/kitokitoki/20110213/p1
+  (defvar my-grep-a-lot-search-word nil)
+  ;;上書き
+  (defun grep-a-lot-buffer-name (position)
+    "Return name of grep-a-lot buffer at POSITION."
+    (concat "*grep*<" my-grep-a-lot-search-word ">"))
+
+  (defadvice rgrep (before my-rgrep (regexp &optional files dir) activate)
+    (setq my-grep-a-lot-search-word regexp))
+
+  (defadvice lgrep (before my-lgrep (regexp &optional files dir) activate)
+    (setq my-grep-a-lot-search-word regexp))
+  )
+
+;; wgrep.el
+;; https://github.com/mhayashi1120/Emacs-wgrep
+(when (require 'wgrep nil t))
+
+;; color-grep.el
+;; http://www.bookshelf.jp/soft/meadow_51.html#SEC778
+;; http://www.bookshelf.jp/elc/color-grep.el
+(when (require 'color-grep nil t)
+  ;; grep バッファを kill 時に，開いたバッファを消す
+  (setq color-grep-sync-kill-buffer t))
+
+(when (require 'ack-and-a-half nil t)
+  ;; Create shorter aliases
+  (defalias 'ack 'ack-and-a-half)
+  (defalias 'ack-same 'ack-and-a-half-same)
+  (defalias 'ack-find-file 'ack-and-a-half-find-file)
+  (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+  )
+
+(provide 'init-grep)
