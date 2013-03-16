e7c5b7a26bd67f22a40ed4726f52b99ba3b83116
masaaki1001 <masa1001@gmail.com>
2013-03-16 23:30:35 +0900

    init-markdown

---
+++ config/init-markdown.el
@@ -0,0 +1,17 @@
+(add-to-list 'auto-mode-alist '("\\.\\(md\\|mdt\\|mdwn\\)$" . markdown-mode))
+(defvar markdown-imenu-generic-expression
+  '(("title"  "^\\(.+?\\)[\n]=+$" 1)
+    ("h2-"    "^\\(.+?\\)[\n]-+$" 1)
+    ("h1"   "^#\\s-+\\(.+?\\)$" 1)
+    ("h2"   "^##\\s-+\\(.+?\\)$" 1)
+    ("h3"   "^###\\s-+\\(.+?\\)$" 1)
+    ("h4"   "^####\\s-+\\(.+?\\)$" 1)
+    ("h5"   "^#####\\s-+\\(.+?\\)$" 1)
+    ("h6"   "^######\\s-+\\(.+?\\)$" 1)
+    ("fn"   "^\\[\\^\\(.+?\\)\\]" 1) ))
+
+(defun my/markdown-mode-hook ()
+  (setq imenu-generic-expression markdown-imenu-generic-expression)
+  (add-to-list 'ac-sources 'ac-source-look))
+
+(provide 'init-markdown)
--- env.el
+++ env.el
@@ -5,6 +5,10 @@
 (setq exec-path (cons "~/.nvm/v0.8.18/bin" exec-path))
 (setenv "PATH" (concat "~/.nvm/v0.8.18/bin:" (getenv "PATH")))
 
+;; markdown
+(setq markdown-command "/usr/local/bin/markdown")
+
+
 ;; load environment value
 ;; http://d.hatena.ne.jp/syohex/20111117/1321503477
 (load-file (expand-file-name "~/.emacs.d/shellenv.el"))
