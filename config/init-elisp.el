01ea32843ebbb50cb87e17cd30142d70a972f08d
masaaki1001 <masa1001@gmail.com>
2013-03-16 23:29:39 +0900

    highlight-symbol

--- config/init-elisp.el
+++ config/init-elisp.el
@@ -78,6 +78,17 @@
   (volatile-highlights-mode t)
   )
 
+(when (require 'highlight-symbol nil t)
+  (defun highlight-symbol-all (&optional nlines)
+    "Call `all' with the symbol at point.
+ Each line is displayed with NLINES lines before and after, or -NLINES
+ before if NLINES is negative."
+    (interactive "P")
+    (interactive "P")  (if (thing-at-point 'symbol)
+                           (all (highlighht-symbol-get-symbol) nlines)
+                         (error "No symbol at point")))
+  )
+
 ;; open-junk-file.el
 (when (require 'open-junk-file nil t)
   (setq open-junk-file-format "~/.emacs.d/junk/%Y-%m-%d-%H%M%S.")
--- config/init-smartrep.el
+++ config/init-smartrep.el
@@ -44,6 +44,31 @@
          ("C-n" . 'next-error)
          ("C-p" . 'previous-error))))
 
+  (defun highlight-symbol-at-point-smartrep ()
+    (interactive)
+    ;; highlight-symbol-at-pointより抜粋
+    (let ((symbol (highlight-symbol-get-symbol)))
+      (unless symbol (error "No symbol at point"))
+      (when (and font-lock-mode (not (highlight-symbol-symbol-highlighted-p symbol)))
+        (highlight-symbol-add-symbol symbol)))
+    ;; smartrep化するコマンドを増やす
+    (message "[p]rev [n]ext [P]revDefun [N]extDefun [L]ist [a]ll [x]Remove [X]RemoveAll")
+    (condition-case e
+        (smartrep-read-event-loop
+         '(("p" . highlight-symbol-prev)
+           ("n" . highlight-symbol-next)
+           ("s" . highlight-symbol-next)
+           ("r" . highlight-symbol-prev)
+           ("C-s" . highlight-symbol-next)
+           ("C-r" . highlight-symbol-prev)
+           ("P" . highlight-symbol-prev-in-defun)
+           ("N" . highlight-symbol-next-in-defun)
+           ("X" . highlight-symbol-remove-all)
+           ("L" . highlight-symbol-list-all)
+           ("a" . highlight-symbol-all)
+           ("x" . highlight-symbol-at-point)))
+      (quit (highlight-symbol-at-point))))
+  (global-set-key (kbd "C-c s") 'highlight-symbol-at-point-smartrep)
   )
 
 (provide 'init-smartrep)
