06ccc9dee23cf26c0e01ca27ce01c9defc7aeff9
masaaki1001 <masa1001@gmail.com>
2013-03-17 09:30:59 +0900

    add init-log

---
+++ config/init-log.el
@@ -0,0 +1,28 @@
+;; session.el
+;; http://maruta.be/intfloat_staff/101
+(when (require 'session nil t)
+  (add-hook 'after-init-hook 'session-initialize)
+  (setq session-save-file "~/.emacs.d/resource/.session")
+  (setq session-save-print-spec '(t nil nil))
+  )
+
+;; savekill.el
+;; http://d.hatena.ne.jp/rubikitch/20110226/
+(when (require 'savekill nil t)
+  (setq save-kill-file-name "~/.emacs.d/resource/kill-ring-saved.el"))
+
+;; scratch-log.el
+;; http://d.hatena.ne.jp/kitokitoki/20100612/p1
+;; https://github.com/wakaran/scratch-log
+(when (require 'scratch-log nil t)
+  (setq sl-scratch-log-file "~/.emacs.d/resource/.scratch-log")
+  (setq sl-prev-scratch-string-file "~/.emacs.d/resource/.scratch-log-prev")
+  (setq sl-restore-scratch-p t)
+  )
+
+;; open-junk-file.el
+(when (require 'open-junk-file nil t)
+  (setq open-junk-file-format "~/.emacs.d/junk/%Y-%m-%d-%H%M%S.")
+  )
+
+(provide 'init-log)
