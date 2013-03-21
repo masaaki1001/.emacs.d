f1a01a4e23698c1c939f95500ec78f6d43d9905c
masaaki1001 <masa1001@gmail.com>
2013-03-21 22:31:38 +0900

    auto-install-update-emacswiki-package-name

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -99,7 +99,7 @@
 ;;----------------------------------------------------------------------------
 (when (require 'auto-install nil t)
  (setq auto-install-directory "~/.emacs.d/auto-install/")
- (auto-install-update-emacswiki-package-name t)
+ ;; (auto-install-update-emacswiki-package-name t)
  (auto-install-compatibility-setup)             ; 互換性確保
  )
 
