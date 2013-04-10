72153397e21d616ae13d8b3633cba3200fc4e11d
masaaki1001 <masa1001@gmail.com>
2013-04-10 20:36:12 +0900

    anything周り

--- config/init-anything.el
+++ config/init-anything.el
@@ -22,27 +22,6 @@
   (define-key anything-map (kbd "C-;") 'abort-recursive-edit)
   )
 
-;; anything-c-moccur.el
-;; http://d.hatena.ne.jp/IMAKADO/20080724/1216882563
-(when (require 'anything-c-moccur nil t)
-  ;;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
-  (setq anything-c-moccur-anything-idle-delay 0.2 ;`anything-idle-delay'
-        anything-c-moccur-higligt-info-line-flag t ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
-        anything-c-moccur-enable-auto-look-flag t ; 現在選択中の候補の位置を他のwindowに表示する
-        anything-c-moccur-enable-initial-pattern nil) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
-
-;;; キーバインドの割当(好みに合わせて設定してください)
-  (global-set-key (kbd "C-o") 'anything-c-moccur-occur-by-moccur) ;バッファ内検索
-  (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur) ;ディレクトリ
-  (add-hook 'dired-mode-hook ;dired
-            '(lambda ()
-               (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
-  )
-
-;; anything-replace-string.el
-;; http://emacs.g.hatena.ne.jp/k1LoW/20110107/1294408979
-(require 'anything-replace-string nil t)
-
 ;; anything-project.el
 ;; http://d.hatena.ne.jp/yuheiomori0718/20111226/1324902529
 (when (require 'anything-project nil t)
