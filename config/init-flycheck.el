7d68559ace426d249e4f0e1cb09b4260fa0d65d8
masaaki1001 <masa1001@gmail.com>
2013-08-27 03:20:37 +0900

    add init-flycheck.el

---
+++ config/init-flycheck.el
@@ -0,0 +1,5 @@
+;; flycheck
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+(add-hook 'css-mode-hook 'flycheck-mode)
+
+(provide 'init-flycheck)
--- config/init-flymake.el
+++ config/init-flymake.el
@@ -58,8 +58,4 @@
   (ad-activate 'flymake-post-syntax-check)
   )
 
-;; flycheck
-(add-hook 'ruby-mode-hook 'flycheck-mode)
-(add-hook 'css-mode-hook 'flycheck-mode)
-
 (provide 'init-flymake)
