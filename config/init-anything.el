8cb3dd41c47df9a215d170045da943941dcf4140
masaaki1001 <masa1001@gmail.com>
2013-10-14 06:20:27 +0900

    modify anything

--- config/init-anything.el
+++ config/init-anything.el
@@ -1,25 +1,26 @@
 ;;----------------------------------------------------------------------------
 ;; anything.el
 ;;----------------------------------------------------------------------------
-(when (require 'anything-startup nil t)
-  (require 'anything-config nil t)
-  (setq anything-sources
-        '(anything-c-source-buffers+
-          anything-c-source-recentf
-          anything-c-source-files-in-current-dir
-          anything-c-source-emacs-commands
-          ))
+(when (require 'anything-config nil t)
+  ;; (setq anything-sources
+  ;;       '(anything-c-source-buffers+
+  ;;         anything-c-source-recentf
+  ;;         anything-c-source-files-in-current-dir
+  ;;         anything-c-source-emacs-commands
+  ;;         ))
 
   (setq anything-samewindow nil)
   (setq anything-candidate-number-limit 300) ; 表示する最大候補数。デフォルトで 50
-  (anything-read-string-mode 1)
+
+  (define-key anything-map (kbd "C-p")   'anything-previous-line)
+  (define-key anything-map (kbd "C-n")   'anything-next-line)
 
   ;; anything 起動
   ;; http://d.hatena.ne.jp/tomoya/20090423/1240456834
-  (define-key global-map (kbd "C-;") 'anything) ; そのお隣り
+  ;; (define-key global-map (kbd "C-;") 'anything) ; そのお隣り
   ;; anything 2重起動エラー対応
   ;; http://d.hatena.ne.jp/yuheiomori0718/20120119/1326976493
-  (define-key anything-map (kbd "C-;") 'abort-recursive-edit)
+  ;; (define-key anything-map (kbd "C-;") 'abort-recursive-edit)
 
   (require 'anything-git-files nil t)
   )
