e1770af1f4e03d9d2aaf81f1f34119bcc07c9fc6
masaaki1001 <masa1001@gmail.com>
2013-12-29 09:52:37 +0900

    edit init-elpa.el

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -4,161 +4,142 @@
 ;;----------------------------------------------------------------------------
 (require 'package)
 
-
-(defvar gnu '("gnu" . "http://elpa.gnu.org/packages/"))
-(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
-(defvar melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(add-to-list 'package-archives gnu t)
-(add-to-list 'package-archives marmalade t)
-(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
-;; https://github.com/magnars/.emacs.d/blob/master/setup-package.el
-(defun packages-install (&rest packages)
-  (mapc (lambda (package)
-          (let ((name (car package))
-                (repo (cdr package)))
-            (when (not (package-installed-p name))
-              (let ((package-archives (list repo)))
-                (package-initialize)
-                (package-install name)))))
-        packages)
-  (package-initialize)
-  (delete-other-windows))
+(package-refresh-contents)
 
-(defun init-install-packages ()
-  (packages-install
-   (cons 'cl-lib gnu)
-   (cons 'melpa melpa)
-   (cons 'auto-install melpa)
-   (cons 'auto-complete melpa)
-   (cons 'fuzzy melpa)
-   (cons 'popwin melpa)
-   (cons 'ac-ispell melpa)
-   (cons 'bm melpa)
-   (cons 'robe melpa)
-   (cons 'ruby-electric melpa)
-   (cons 'ruby-block melpa)
-   (cons 'inf-ruby melpa)
-   (cons 'ruby-interpolation melpa)
-   (cons 'rbenv melpa)
-   (cons 'rspec-mode melpa)
-   (cons 'rinari melpa)
-   (cons 'bundler melpa)
-   (cons 'rhtml-mode melpa)
-   (cons 'direx melpa)
-   ;; (cons 'dsvn melpa)
-   (cons 'rainbow-mode gnu)
-   (cons 'goto-chg melpa)
-   (cons 'maxframe melpa)
-   (cons 'color-moccur melpa)
-   (cons 'diminish melpa)
-   (cons 'magit melpa)
-   (cons 'magit-svn melpa)
-   (cons 'magit-find-file melpa)
-   (cons 'git-commit-mode melpa)
-   (cons 'gitconfig-mode melpa)
-   (cons 'gitignore-mode melpa)
-   (cons 'git-messenger melpa)
-   (cons 'git-rebase-mode melpa)
-   (cons 'gh melpa)
-   (cons 'elscreen melpa)
-   (cons 'flycheck melpa)
-   (cons 'undohist melpa)
-   (cons 'point-undo melpa)
-   (cons 'quickrun melpa)
-   (cons 'all gnu)
-   (cons 'all-ext melpa)
-   (cons 'replace-from-region melpa)
-   (cons 'expand-region melpa)
-   (cons 'multiple-cursors melpa)
-   (cons 'redo+ melpa)
-   (cons 'bash-completion melpa)
-   (cons 'yagist melpa)
-   (cons 'scala-mode2 melpa)
-   (cons 'ensime melpa)
-   (cons 'yaml-mode melpa)
-   (cons 'anything melpa)
-   (cons 'anything-git-files melpa)
-   (cons 'helm melpa)
-   (cons 'helm-swoop melpa)
-   (cons 'helm-projectile melpa)
-   (cons 'helm-ls-git melpa)
-   (cons 'helm-c-yasnippet melpa)
-   (cons 'helm-open-github melpa)
-   (cons 'helm-spaces melpa)
-   (cons 'helm-git-grep melpa)
-   (cons 'helm-dired-recent-dirs melpa)
-   (cons 'helm-ag melpa)
-   (cons 'helm-ag-r melpa)
-   (cons 'helm-descbinds melpa)
-   (cons 'helm-rails melpa)
-   (cons 'helm-rb melpa)
-   (cons 'helm-bm melpa)
-   (cons 'helm-dash melpa)
-   (cons 'htmlize melpa)
-   (cons 'emmet-mode melpa)
-   (cons 'markdown-mode melpa)
-   (cons 'markdown-mode+ melpa)
-   (cons 'color-moccur melpa)
-   (cons 'scratch-log melpa)
-   (cons 'session melpa)
-   (cons 'savekill melpa)
-   (cons 'sequential-command melpa)
-   (cons 'duplicate-thing melpa)
-   (cons 'open-junk-file melpa)
-   (cons 'recentf-ext melpa)
-   (cons 'viewer melpa)
-   (cons 'dash-at-point melpa)
-   (cons 'elisp-slime-nav melpa)
-   (cons 'eldoc-extension melpa)
-   (cons 'paredit melpa)
-   (cons 'smartparens melpa)
-   (cons 'spaces melpa)
-   (cons 'e2wm melpa)
-   (cons 'window-layout melpa)
-   (cons 'switch-window melpa)
-   (cons 'jaunte melpa)
-   (cons 'ace-jump-mode melpa)
-   (cons 'imenu-anywhere melpa)
-   (cons 'less-css-mode melpa)
-   (cons 'scss-mode melpa)
-   (cons 'css-eldoc melpa)
-   (cons 'json-mode melpa)
-   (cons 'js2-mode melpa)
-   (cons 'tss melpa)
-   (cons 'jquery-doc melpa)
-   (cons 'nodejs-repl melpa)
-   (cons 'coffee-mode melpa)
-   (cons 'wgrep melpa)
-   (cons 'wgrep-ag melpa)
-   (cons 'grep-a-lot melpa)
-   (cons 'ag melpa)
-   (cons 'smartrep melpa)
-   (cons 'pomodoro melpa)
-   (cons 'zlc melpa)
-   (cons 'revive melpa)
-   (cons 'exec-path-from-shell melpa)
-   (cons 'volatile-highlights melpa)
-   (cons 'highlight-symbol melpa)
-   (cons 'undo-tree melpa)
-   (cons 'yasnippet melpa)
-   (cons 'dropdown-list melpa)
-   (cons 'web-mode melpa)
-   (cons 'w3m melpa)
-   (cons 'migemo melpa)
-   (cons 'anzu melpa)
-   (cons 'move-text melpa)
-   (cons 'highlight-escape-sequences melpa)
-   ))
+(defvar init-install-packages
+  '(
+    cl-lib
+    auto-install
+    auto-complete
+    fuzzy
+    popwin
+    ac-ispell
+    bm
+    robe
+    ruby-electric
+    ruby-block
+    inf-ruby
+    ruby-interpolation
+    rbenv
+    rspec-mode
+    rinari
+    bundler
+    rhtml-mode
+    direx
+    ;; dsvn
+    rainbow-mode
+    goto-chg
+    maxframe
+    color-moccur
+    diminish
+    magit
+    magit-svn
+    magit-find-file
+    git-commit-mode
+    gitconfig-mode
+    gitignore-mode
+    git-messenger
+    git-rebase-mode
+    gh
+    elscreen
+    flycheck
+    undohist
+    point-undo
+    quickrun
+    all
+    all-ext
+    replace-from-region
+    expand-region
+    multiple-cursors
+    redo+
+    bash-completion
+    yagist
+    scala-mode2
+    ensime
+    yaml-mode
+    anything
+    anything-git-files
+    helm
+    helm-swoop
+    helm-projectile
+    helm-ls-git
+    helm-c-yasnippet
+    helm-open-github
+    helm-spaces
+    helm-git-grep
+    helm-dired-recent-dirs
+    helm-ag
+    helm-ag-r
+    helm-descbinds
+    helm-rails
+    helm-rb
+    helm-bm
+    helm-dash
+    htmlize
+    emmet-mode
+    markdown-mode
+    markdown-mode+
+    color-moccur
+    scratch-log
+    session
+    savekill
+    sequential-command
+    duplicate-thing
+    open-junk-file
+    recentf-ext
+    viewer
+    dash-at-point
+    elisp-slime-nav
+    eldoc-extension
+    paredit
+    smartparens
+    spaces
+    e2wm
+    window-layout
+    switch-window
+    jaunte
+    ace-jump-mode
+    imenu-anywhere
+    less-css-mode
+    scss-mode
+    css-eldoc
+    json-mode
+    js2-mode
+    tss
+    jquery-doc
+    nodejs-repl
+    coffee-mode
+    wgrep
+    wgrep-ag
+    grep-a-lot
+    ag
+    smartrep
+    pomodoro
+    zlc
+    revive
+    exec-path-from-shell
+    volatile-highlights
+    highlight-symbol
+    undo-tree
+    yasnippet
+    dropdown-list
+    web-mode
+    w3m
+    migemo
+    anzu
+    move-text
+    highlight-escape-sequences
+    ))
 
-(condition-case nil
-    (init-install-packages)
-  (error
-   (package-refresh-contents)
-   (init-install-packages)))
+(dolist (package init-install-packages)
+  (when (not (package-installed-p package))
+    (package-install package)))
 
 ;; auto-install.el
 (when (require 'auto-install nil t)
