3dc491c61f6aa1b44edb4ec04109f9b34886ddd7
masaaki1001 <masa1001@gmail.com>
2013-12-31 23:09:49 +0900

    delete submodule

--- config/init-anything.el
+++ config/init-anything.el
@@ -1,7 +1,7 @@
 ;;;; anything.el
 (when (require 'anything-config nil t)
   (setq anything-samewindow nil)
-  (setq anything-candidate-number-limit 300) ; 表示する最大候補数。デフォルトで 50
+  (setq anything-candidate-number-limit 300)
 
   (define-key anything-map (kbd "C-p")   'anything-previous-line)
   (define-key anything-map (kbd "C-n")   'anything-next-line)
