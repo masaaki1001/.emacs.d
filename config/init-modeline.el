0bb6af25aff55b2f26546e0eea49a6d58cbd497e
watanabe <m-watanabe@iij.ad.jp>
2014-01-09 12:28:23 +0900

    modify diminish

--- config/init-modeline.el
+++ config/init-modeline.el
@@ -1,12 +1,12 @@
 ;;;; diminish.el
 (when (require 'diminish nil t)
-  (eval-after-load "auto-complete-mode" '(diminish 'auto-complete-mode))
+  (eval-after-load "auto-complete" '(diminish 'auto-complete-mode))
   (eval-after-load "yasnippet" '(diminish 'yas-minor-mode))
-  (eval-after-load "undo-tree-mode" '(diminish 'undo-tree-mode))
-  (eval-after-load "volatile-highlights-mode" '(diminish 'volatile-highlights-mode))
-  (eval-after-load "view-mode" '(diminish 'view-mode))
+  (eval-after-load "undo-tree" '(diminish 'undo-tree-mode))
+  (eval-after-load "volatile-highlights" '(diminish 'volatile-highlights-mode))
+  (eval-after-load "view" '(diminish 'view-mode))
   (eval-after-load "elisp-slime-nav" '(diminish 'elisp-slime-nav-mode))
-  (eval-after-load "anzu-mode" '(diminish 'anzu-mode))
+  (eval-after-load "anzu"'(diminish 'anzu-mode))
   (eval-after-load "smartparens" '(diminish 'smartparens-mode))
   )
 
