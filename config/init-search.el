274d4ecef262733ae959c4ece16692f799473331
masaaki1001 <masa1001@gmail.com>
2014-01-26 15:27:43 +0900

    added isearch-yank-symbol

--- config/init-search.el
+++ config/init-search.el
@@ -2,6 +2,11 @@
 ;; i-searchでのbackspace有効
 (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
 
+(defun isearch-yank-symbol ()
+  (interactive)
+  (isearch-yank-internal (lambda () (forward-symbol 1) (point))))
+(define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
+
 (when (require 'anzu nil t)
   (global-anzu-mode t)
   (set-face-attribute 'anzu-mode-line nil
