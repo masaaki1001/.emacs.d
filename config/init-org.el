81d38264467caba337394114d5500ca66b26f04f
masaaki1001 <masa1001@gmail.com>
2014-04-11 18:54:21 +0900

    modify init-org

--- config/init-org.el
+++ config/init-org.el
@@ -2,28 +2,27 @@
 ;; http://d.hatena.ne.jp/rubikitch/20090121/1232468026
 ;; http://e-arrows.sakura.ne.jp/2010/02/vim-to-emacs.html
 (when (require 'org-install nil t)
-  (define-key global-map (kbd "C-c l") 'org-store-link)
-  (define-key global-map (kbd "C-c p") 'org-capture)
   (setq org-startup-truncated nil)
   (setq org-return-follows-link t)
   (setq org-use-fast-todo-selection t)
-  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
   (setq org-directory "~/.emacs.d/org-mode/")
 
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")
-          (sequence "APPT(a)" "|" "DONE(x)" "CANCEL(c)")))
-  (setq org-log-done 'time)
-)
+  (global-set-key (kbd "C-c l") 'org-store-link)
+  (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c a") 'org-agenda)
 
-;; org-tree-slide.el
-;; http://pastelwill.jp/wiki/doku.php?id=emacs:org-tree-slide
-;; https://github.com/takaxp/org-tree-slide
-(when (require 'org-tree-slide nil t)
-  (global-set-key (kbd "<f6>") 'org-tree-slide-mode)
-  (global-set-key (kbd "S-<f6>") 'org-tree-slide-skip-done-toggle)
-  ;; エフェクト無効化
-  (org-tree-slide-simple-profile)
+  (setq org-agenda-files '("~/.emacs.d/org-mode/todo.org"))
+  (setq org-log-done 'time)
+
+  ;; org-tree-slide.el
+  ;; http://pastelwill.jp/wiki/doku.php?id=emacs:org-tree-slide
+  ;; https://github.com/takaxp/org-tree-slide
+  (when (require 'org-tree-slide nil t)
+    (global-set-key (kbd "<f6>") 'org-tree-slide-mode)
+    (global-set-key (kbd "S-<f6>") 'org-tree-slide-skip-done-toggle)
+    ;; エフェクト無効化
+    (org-tree-slide-simple-profile)
+    )
   )
 
 (provide 'init-org)
