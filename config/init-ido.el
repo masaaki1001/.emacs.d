9202d11426f8e60d014ffe388a98b7a7c2b24bf3
masaaki1001 <masa1001@gmail.com>
2014-05-12 09:32:06 +0900

    init-ido

--- config/init-ido.el
+++ config/init-ido.el
@@ -5,13 +5,20 @@
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-use-filename-at-point nil
+      ido-use-faces nil
       ido-max-prospects 10)
 
 (when (require 'ido-ubiquitous nil t)
   (ido-ubiquitous-mode t))
-(when (require 'smex nil t)
-  (global-set-key [remap execute-extended-command] 'smex))
+
 (when (require 'ido-vertical-mode nil t)
   (ido-vertical-mode))
+
+(when (require 'flx-ido nil t)
+  (flx-ido-mode t))
+
+(when (require 'smex nil t)
+  (global-set-key [remap execute-extended-command] 'smex))
+
 
 (provide 'init-ido)
