e88889057d32fa113317d79890ea09a922f6eea0
masaaki1001 <masa1001@gmail.com>
2014-05-17 19:07:33 +0900

    typescript

--- config/init-typescript.el
+++ config/init-typescript.el
@@ -1,15 +1,20 @@
 ;;;; typescript
 (when (require 'typescript nil t)
   (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
+  (setq typescript-auto-indent-flag nil)
+  (define-key typescript-mode-map (kbd "C-c C-j") nil)
+
+  (eval-after-load "typescript"
+    (setq require-final-newline t)
+    )
 
   (when (require 'tss nil t)
     (setq tss-popup-help-key "C-c C-c t")
     (setq tss-jump-to-definition-key "C-c C-c C-t")
 
     (tss-config-default)
-    (setq typescript-auto-indent-flag nil)
-
-    (define-key typescript-mode-map (kbd "C-c C-j") nil))
+    (remove-hook 'after-save-hook 'tss-run-flymake)
+    )
   )
 
 (provide 'init-typescript)
