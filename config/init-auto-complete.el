6ba34d02acb234d7920e48827aaf6c3cb6662db6
masaaki1001 <masa1001@gmail.com>
2014-05-26 15:54:08 +0900

    auto-complete

--- config/init-auto-complete.el
+++ config/init-auto-complete.el
@@ -8,18 +8,23 @@
                (expand-file-name  "ac-dict" user-emacs-directory))
   (setq ac-comphist-file (expand-file-name "ac-comphist.dat" resource-dir))
   (ac-config-default)
+  (setq-default ac-sources (append '(ac-source-words-in-buffer
+                                     ac-source-words-in-all-buffer
+                                     ac-source-imenu) ac-sources))
   (setq ac-delay 0.5
         ac-use-quick-help t
         ac-quick-help-delay 1.0
         ac-use-menu-map t
         ac-auto-start t
         ac-ignore-case nil)
+
   (add-hook 'auto-complete-mode-hook
             (lambda ()
               (define-key ac-completing-map (kbd "C-n") 'ac-next)
               (define-key ac-completing-map (kbd "C-p") 'ac-previous)
               (define-key ac-completing-map (kbd "C-s") 'ac-isearch)
               (define-key ac-mode-map (kbd "M-RET") 'auto-complete)
+              ;; (define-key ac-mode-map (kbd "M-/") 'auto-complete)
               ))
 
   (eval-after-load "yasnippet"
@@ -34,6 +39,7 @@
                   sass-mode
                   html-mode
                   web-mode
+                  typescript-mode
                   ))
     (add-to-list 'ac-modes mode))
 
