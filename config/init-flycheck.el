d92ff5f6c3861f16df6017a3f245199168b648cf
masaaki1001 <masa1001@gmail.com>
2014-06-03 11:10:42 +0900

    flycheck

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -1,5 +1,7 @@
 ;;;; flycheck
 (add-hook 'ruby-mode-hook 'flycheck-mode)
 (add-hook 'css-mode-hook 'flycheck-mode)
+(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+      flycheck-idle-change-delay 10)
 
 (provide 'init-flycheck)
