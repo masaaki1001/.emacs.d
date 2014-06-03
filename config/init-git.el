6ae3ace0c9b1d203e0ba5283a4b32cd55b3513b8
masaaki1001 <masa1001@gmail.com>
2014-06-03 22:50:35 +0900

    http://qiita.com/onjiro@github/items/15eda8d539937a9991f5

--- config/init-git.el
+++ config/init-git.el
@@ -3,13 +3,13 @@
   (require 'magit-blame nil t)
   (require 'magit-svn nil t)
 
+  ;; 72文字折り返しを無効化
+  (add-hook 'git-commit-mode-hook 'turn-off-auto-fill)
+
   ;; disable vc-mode
   (setq vc-handled-backends '())
   (eval-after-load "vc"
     '(remove-hook 'find-file-hooks 'vc-find-file-hook))
-
-    ;; 72文字折り返しを無効化
-  (add-hook 'git-commit-mode-hook 'turn-off-auto-fill)
 
   ;; http://d.hatena.ne.jp/syohex/20130201/1359731697
   (let ((cell (or (memq 'mode-line-position mode-line-format)
