064fe54a78ff16e08b9b0ed143bcbe69de20dbd9
masaaki1001 <masa1001@gmail.com>
2014-06-08 22:22:18 +0900

    elscreen

--- config/init-window.el
+++ config/init-window.el
@@ -1,8 +1,16 @@
 ;;;; window
 (require 'spaces nil t)
 
-(defadvice elscreen-jump (around elscreen-last-command-char-event activate)
-  (let ((last-command-char last-command-event))
-    ad-do-it))
+
+(when (require 'elscreen nil t)
+  (defadvice elscreen-jump (around elscreen-last-command-char-event activate)
+    (let ((last-command-char last-command-event))
+      ad-do-it))
+  )
+
+(when (require 'zoom-window nil t)
+  (zoom-window-setup)
+  (global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
+  )
 
 (provide 'init-window)
