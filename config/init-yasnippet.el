b3c5c17a0944c7052b221be363d6651163b29b95
masaaki1001 <masa1001@gmail.com>
2014-06-23 11:23:10 +0900

    自分の定義したsnippetのみにする

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -3,6 +3,10 @@
   (yas-global-mode 1)
   ;; (custom-set-variables '(yas-trigger-key "TAB"))
 
+  ;; 自分の定義したsnippetのみにする．
+  (setq-default yas-snippet-dirs (expand-file-name  "snippets" user-emacs-directory))
+  ;; (setq yas-snippet-dirs (expand-file-name  "snippets" user-emacs-directory))
+
   (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
   (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
   (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
