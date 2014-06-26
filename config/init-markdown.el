b318f22d049e77ef6a200949d361bcf9bfe99b91
masaaki1001 <masa1001@gmail.com>
2014-06-26 11:50:57 +0900

    add markdown-css

--- config/init-markdown.el
+++ config/init-markdown.el
@@ -15,6 +15,12 @@
   (add-hook 'markdown-mode-hook
             (lambda ()
               (setq imenu-generic-expression markdown-imenu-generic-expression)))
+
+  ;; markdown-css
+  (setq github-markdown-style-dir (expand-file-name "github-css" repositories-dir))
+  (setq markdown-css-themes-dir (expand-file-name "markdown-css-themes" repositories-dir))
+
+  (setq markdown-css-path (expand-file-name "github.css" github-markdown-style-dir))
   )
 
 (provide 'init-markdown)
