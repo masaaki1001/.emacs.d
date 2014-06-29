25b2c1c154e2f73103948c7529d6d2991e07598c
masaaki1001 <masa1001@gmail.com>
2014-06-29 22:04:33 +0900

    zoom-window

--- config/init-window.el
+++ config/init-window.el
@@ -1,15 +1,21 @@
 ;;;; window
 (require 'spaces nil t)
 
-
 (when (require 'elscreen nil t)
   (defadvice elscreen-jump (around elscreen-last-command-char-event activate)
     (let ((last-command-char last-command-event))
-      ad-do-it))
-  )
+      ad-do-it)))
 
 (when (require 'zoom-window nil t)
-  (zoom-window-setup)
+  ;; M-x elscreen-start 実行時に一度だけ実行させる．
+  (defun my/elscreen-update-hook ()
+    (setq zoom-window-use-elscreen t)
+    (zoom-window-setup)
+    (remove-hook 'elscreen-screen-update-hook 'my/elscreen-update-hook)
+    (message "zoom-window-setup...done"))
+
+  (add-hook 'elscreen-screen-update-hook 'my/elscreen-update-hook)
+
   (global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
   )
 
