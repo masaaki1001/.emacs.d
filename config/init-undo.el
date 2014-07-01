01c5485aaca2da707e25eef4416552fe1ae60a01
masaaki1001 <masa1001@gmail.com>
2014-07-01 21:18:47 +0900

    move undo setting

--- config/init-undo.el
+++ config/init-undo.el
@@ -1,4 +1,7 @@
 ;;;; undo
+(setq undo-no-redo t) ; 過去のundoがredoされないようにする
+(setq undo-limit 600000)
+(setq undo-strong-limit 900000)
 ;; undo-tree.el
 ;; http://www.dr-qubit.org/undo-tree/undo-tree.el
 (when (require 'undo-tree nil t)
@@ -10,9 +13,6 @@
 
 ;; redo+
 (when (require 'redo+ nil t)
-  (global-set-key (kbd "C-M-/") 'redo)
-  (setq undo-no-redo t) ; 過去のundoがredoされないようにする
-  (setq undo-limit 600000)
-  (setq undo-strong-limit 900000))
+  (global-set-key (kbd "C-M-/") 'redo))
 
 (provide 'init-undo)
