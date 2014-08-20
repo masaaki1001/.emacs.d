02a4080da59d5a9e9e378eb785294da620dd1642
masaaki1001 <masa1001@gmail.com>
2014-08-20 10:34:14 +0900

    delete auto-mode-alias

--- config/init-html.el
+++ config/init-html.el
@@ -22,10 +22,7 @@
 (when (require 'web-mode nil t)
   (setq web-mode-markup-indent-offset 2)
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (define-key web-mode-map (kbd "C-;") nil)
   ;; web-modeでのjshint実行キーバインドを変更
