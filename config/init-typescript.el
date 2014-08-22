50ed95bcd6bda2e3e7a3622b57808fa539122d89
masaaki1001 <masa1001@gmail.com>
2014-08-22 17:18:33 +0900

    code format

--- config/init-typescript.el
+++ config/init-typescript.el
@@ -17,8 +17,7 @@
     (remove-hook 'after-save-hook 'tss-run-flymake)
     (setq ac-sources (append '(ac-source-words-in-buffer
                                ac-source-words-in-all-buffer
-                               ac-source-imenu) ac-sources))
-    )
+                               ac-source-imenu) ac-sources)))
 
   (eval-after-load "flycheck"
     '(progn
