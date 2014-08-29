1a0a8cdf509402972641cbbfe7ae6f9d1b3e950d
masaaki1001 <masa1001@gmail.com>
2014-08-29 09:08:00 +0900

    delete multi-term

--- config/init-shell.el
+++ config/init-shell.el
@@ -1,8 +1,4 @@
 ;;;; shell
-;; multi-term
-(when (require 'multi-term nil t)
-  (setq multi-term-program "/bin/bash"))
-
 ;; emacs-bash-completion
 (when (require 'bash-completion nil t)
   (bash-completion-setup))
