36d50514e92ecef037355a9cbe17252d41067ad4
masaaki1001 <masa1001@gmail.com>
2014-09-12 10:49:23 +0900

    bm

--- config/init-bm.el
+++ config/init-bm.el
@@ -4,32 +4,36 @@
 (require 'bm)
 ;; マークのセーブ
 (setq-default bm-buffer-persistence t)
-(setq bm-repository-file (expand-file-name ".bm-repository" resource-dir))
 ;; 起動時に設定のロード
-(setq bm-restore-repository-on-load t)
-(setq bm-highlight-style 'bm-highlight-only-fringe)
-(setq bm-marker 'bm-marker-right)
-(setq bm-cycle-all-buffers t)
-(add-hook 'after-init-hook '(lambda nil
-                              (bm-load-and-restore)
-                              (message "Load and Restore bookmarks...done")))
+(setq bm-repository-file (expand-file-name ".bm-repository" resource-dir)
+      bm-restore-repository-on-load t
+      bm-highlight-style 'bm-highlight-only-fringe
+      bm-marker 'bm-marker-right
+      bm-cycle-all-buffers t)
 
-(add-hook 'find-file-hooks 'bm-buffer-restore)
-(add-hook 'after-revert-hook 'bm-buffer-restore)
-;; 設定ファイルのセーブ
-(add-hook 'kill-buffer-hook 'bm-buffer-save)
-(add-hook 'auto-save-hook 'bm-buffer-save)
-(add-hook 'after-save-hook 'bm-buffer-save)
-(add-hook 'vc-before-checkin-hook 'bm-buffer-save)
-(add-hook 'after-revert-hook 'bm-buffer-restore)
+(dolist (hook '(kill-buffer-hook
+                auto-save-hook
+                after-save-hook
+                vc-before-checkin-hook))
+  (add-hook hook 'bm-buffer-save))
+
+(dolist (hook '(find-file-hooks
+                after-revert-hook
+                after-revert-hook))
+  (add-hook hook 'bm-buffer-restore))
+
+(defun my/bm-after-init-hook ()
+  (bm-load-and-restore)
+  (message "Load and Restore bookmarks...done"))
+(add-hook 'after-init-hook 'my/bm-after-init-hook)
+
+(defun my/bm-kill-emacs-hook ()
+  (bm-save)
+  (message "Save bookmarks...done"))
+(add-hook 'kill-emacs-hook 'my/bm-kill-emacs-hook)
+
 (global-set-key (kbd "<M-f2>") 'bm-toggle)
 (global-set-key (kbd "M-[") 'bm-previous)
 (global-set-key (kbd "M-]") 'bm-next)
-;; Saving the repository to file when on exit.
-;; kill-buffer-hook is not called when emacs is killed, so we
-;; must save all bookmarks first.
-(add-hook 'kill-emacs-hook '(lambda nil
-                              (bm-save)
-                              (message "Save bookmarks...done")))
 
 (provide 'init-bm)
