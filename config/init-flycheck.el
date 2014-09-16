78c207aab9c4d420b9908c5c4113895ba573ae0b
masaaki1001 <masa1001@gmail.com>
2014-09-16 09:23:17 +0900

    move typescript-tslint settings

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -7,12 +7,31 @@
       flycheck-idle-change-delay 10)
 
 (eval-after-load "flycheck"
-  (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+  '(progn
+     (require 'projectile)
+     (defcustom flycheck-tslintrc-file-name "./config/tslint.json"
+       "Customize config file name"
+       :type 'string)
 
-(eval-after-load "flycheck-color-mode-line"
-  '(custom-set-faces
-    '(flycheck-color-mode-line-error-face ((t (:background "red3" :foreground "#efefef" :weight normal))))
-    '(flycheck-color-mode-line-info-face ((t (:inherit (flycheck-fringe-info default) :background "DodgerBlue3" :foreground "#efefef" :weight normal))))
-    '(flycheck-color-mode-line-warning-face ((t (:background "yellow3" :foreground "black" :weight normal))))))
+     (flycheck-def-config-file-var flycheck-tslintrc typescript-tslint flycheck-tslintrc-file-name
+       :safe #'string)
+
+     (flycheck-define-checker typescript-tslint
+       "Use tslint to flycheck TypeScript code."
+       :command ("tslint"
+                 "-f" source
+                 "-c" (eval (projectile-expand-root flycheck-tslintrc-file-name))
+                 "-t" "prose")
+       :error-patterns ((warning (file-name) "[" line ", " column "]: " (message)))
+       :modes typescript-mode)
+
+     (add-to-list 'flycheck-checkers 'typescript-tslint)
+
+     ;; flycheck-color-mode-line
+     (custom-set-faces
+      '(flycheck-color-mode-line-error-face ((t (:background "red3" :foreground "#efefef" :weight normal))))
+      '(flycheck-color-mode-line-info-face ((t (:inherit (flycheck-fringe-info default) :background "DodgerBlue3" :foreground "#efefef" :weight normal))))
+      '(flycheck-color-mode-line-warning-face ((t (:background "yellow3" :foreground "black" :weight normal)))))
+     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
 (provide 'init-flycheck)
