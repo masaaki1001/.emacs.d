d8e628778f1cc4afe58bf7b0d0ad228d11bf3f80
masaaki1001 <masa1001@gmail.com>
2014-09-16 09:24:51 +0900

    wgrep-ag

--- config/init-grep.el
+++ config/init-grep.el
@@ -58,9 +58,7 @@
   '(progn
      (require 'wgrep-ag)
      (custom-set-variables
-      '(ag-highlight-search t)   ; 検索結果の中の検索語をハイライトする
-      '(ag-reuse-window 'nil)    ; 現在のウィンドウを検索結果表示に使う
-      '(ag-reuse-buffers 'nil))  ; 現在のバッファを検索結果表示に使う
+      '(ag-highlight-search t))
      (autoload 'wgrep-ag-setup "wgrep-ag")
      (add-hook 'ag-mode-hook 'wgrep-ag-setup)
      ;; agの検索結果バッファで"r"で編集モードに。
