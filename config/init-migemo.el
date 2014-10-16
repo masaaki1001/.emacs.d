2d9eb0a55c80650e4ffe9a6597e3f229239f4d3c
masaaki1001 <masa1001@gmail.com>
2014-10-16 09:47:15 +0900

    不要な条件を削除

--- config/init-migemo.el
+++ config/init-migemo.el
@@ -1,6 +1,5 @@
 ;; http://d.hatena.ne.jp/samurai20000/20100907/1283791433
-(when (and (executable-find "cmigemo")
-           (locate-library "migemo"))
+(when (executable-find "cmigemo")
   ;; デフォルトではmigemoを無効にする
   ;; http://www.meadowy.org/meadow/netinstall/wiki/PkgMigemo#a.emacs
   (setq migemo-isearch-enable-p nil)
