f7536627c1aa9129a441cd90f45fb8b9b6a46a70
masaaki1001 <masa1001@gmail.com>
2014-12-14 21:43:42 +0900

    anzu-use-migemo

--- config/init-search.el
+++ config/init-search.el
@@ -9,6 +9,7 @@
 (define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
 
 (global-anzu-mode t)
+(setq anzu-use-migemo t)
 (set-face-attribute 'anzu-mode-line nil
                     :foreground "black" :weight 'bold)
 (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
