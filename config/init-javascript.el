95d315a83cef8c8e8c320d2baa0089c2be11b098
masaaki1001 <masa1001@gmail.com>
2015-01-01 17:44:42 +0900

    使っていないものを削除

--- config/init-javascript.el
+++ config/init-javascript.el
@@ -1,6 +1,5 @@
 ;; js2-mode
 (add-hook 'js2-mode-hook '(lambda () (setq mode-name "js2")))
-(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
 (setq js2-auto-indent-p t)
