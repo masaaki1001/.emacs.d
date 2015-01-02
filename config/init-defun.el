7617f8b2a5bf356ff652c6fc500d21f93d018474
masaaki1001 <masa1001@gmail.com>
2015-01-02 13:32:02 +0900

    remove window-toggle-division

--- config/init-defun.el
+++ config/init-defun.el
@@ -1,21 +1,3 @@
-;; 縦分割と横分割を切り替える M-x window-toggle-divisionでできるようにする
-;; http://d.hatena.ne.jp/himadatanode/20061011/p2
-(defun window-toggle-division ()
-  "ウィンドウ 2 分割時に、縦分割<->横分割"
-  (interactive)
-  (unless (= (count-windows 1) 2)
-    (error "ウィンドウが 2 分割されていません。"))
-  (let (before-height (other-buf (window-buffer (next-window))))
-    (setq before-height (window-height))
-    (delete-other-windows)
-
-    (if (= (window-height) before-height)
-        (split-window-vertically)
-      (split-window-horizontally)
-      )
-    (switch-to-buffer-other-window other-buf)
-    (other-window -1)))
-
 (defun kill-all-buffers ()
   "Kill all buffers."
   (interactive)
