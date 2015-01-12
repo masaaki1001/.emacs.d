4d31a6d27cf4a5003608b2e9623a3207d4e38a6b
masaaki1001 <masa1001@gmail.com>
2015-01-12 22:38:48 +0900

    init-helm

--- config/init-helm.el
+++ config/init-helm.el
@@ -7,9 +7,6 @@
       helm-candidate-number-limit 300
       helm-delete-minibuffer-contents-from-point t
       helm-samewindow nil
-
-(helm-descbinds-mode)
-(helm-match-plugin-mode)
       helm-buffer-max-length 50
       helm-M-x-fuzzy-match t)
 
@@ -72,6 +69,10 @@
 ;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+(with-eval-after-load 'helm
+  (helm-descbinds-mode)
+  (helm-match-plugin-mode))
 
 (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
   "Execute command only if CANDIDATE exists"
