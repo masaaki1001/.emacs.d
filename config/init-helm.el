d4918a9cc6ef6b51bee01605c6cd4ce9f2a1ac99
masaaki1001 <masa1001@gmail.com>
2015-03-25 11:03:14 +0900

    helm

--- config/init-helm.el
+++ config/init-helm.el
@@ -50,7 +50,6 @@
 (global-set-key (kbd "C-c C-a") 'helm-ag)
 (global-set-key (kbd "C-c ;") 'helm-ls-git-ls)
 (global-set-key (kbd "C-c y") 'helm-yas-complete)
-(global-set-key (kbd "C-c C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
 ;; (global-set-key [remap execute-extended-command] 'helm-M-x)
@@ -65,25 +64,24 @@
 
 (with-eval-after-load 'helm
   (helm-descbinds-mode)
-  (helm-match-plugin-mode)
 
   (define-key helm-map (kbd "C-p")   'helm-previous-line)
   (define-key helm-map (kbd "C-n")   'helm-next-line)
   (define-key helm-map (kbd "C-M-n") 'helm-next-source)
-  (define-key helm-map (kbd "C-M-p") 'helm-previous-source))
+  (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
 
-;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
+  ;; http://rubikitch.com/2014/12/19/helm-buffers-sort-transformer/
+  (defadvice helm-buffers-sort-transformer (around ignore activate)
+    (setq ad-return-value (ad-get-arg 0)))
+
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end)))))
+
 (with-eval-after-load 'helm-files
-  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action))
-
-(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-  "Execute command only if CANDIDATE exists"
-  (when (file-exists-p candidate)
-    ad-do-it))
-
-(defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
-  "Emulate `kill-line' in helm minibuffer"
-  (kill-new (buffer-substring (point) (field-end))))
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it)))
 
 (provide 'init-helm)
