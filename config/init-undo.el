4e866bbc24d0a7ddee8d125317190a6d4e81f578
masaaki1001 <masa1001@gmail.com>
2015-03-25 11:01:52 +0900

    history

--- config/init-undo.el
+++ config/init-undo.el
@@ -3,11 +3,15 @@
 (setq undo-strong-limit 900000)
 ;; undo-tree
 (global-undo-tree-mode)
+(setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist
+      `((".*" . ,(expand-file-name "undo-tree" resource-dir))))
 (global-set-key (kbd "C-M-/") 'undo-tree-redo)
 
 ;; undohist
 (require 'undohist)
 (undohist-initialize)
 (setq undohist-ignored-files '("COMMIT_EDITMSG"))
+(setq undohist-directory (expand-file-name "undohist" resource-dir))
 
 (provide 'init-undo)
