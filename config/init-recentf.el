ef8afaf51831b71ad34df4a378ad8ea6b8b5029b
masaaki1001 <masa1001@gmail.com>
2015-03-25 11:03:00 +0900

    remove recentf-cleanup

--- config/init-recentf.el
+++ config/init-recentf.el
@@ -4,37 +4,10 @@
 (setq recentf-exclude '("/.recentf" ".revive" "/elpa/" "/snippets/" "COMMIT_EDITMSG"))
 (setq recentf-save-file (expand-file-name ".recentf" resource-dir))
 (setq recentf-max-saved-items 2000)
-(setq recentf-auto-cleanup 10)
-(run-with-idle-timer 30 t 'recentf-save-list)
 (recentf-mode 1)
 
 ;; recentf-ext
 ;; http://d.hatena.ne.jp/rubikitch/20091224/recentf
 (require 'recentf-ext)
-
-;; http://masutaka.net/chalow/2011-10-30-2.html
-(require 'cl)
-(defvar my-recentf-list-prev nil)
-(defadvice recentf-save-list
-  (around no-message activate)
-  "If `recentf-list' and previous recentf-list are equal,
-do nothing. And suppress the output from `message' and
-`write-file' to minibuffer."
-  (unless (equal recentf-list my-recentf-list-prev)
-    (flet ((message (format-string &rest args)
-                    (eval `(format ,format-string ,@args)))
-           (write-file (file &optional confirm)
-                       (let ((str (buffer-string)))
-                         (with-temp-file file
-                           (insert str)))))
-      ad-do-it
-      (setq my-recentf-list-prev recentf-list))))
-
-(defadvice recentf-cleanup
-  (around no-message activate)
-  "suppress the output from `message' to minibuffer"
-  (flet ((message (format-string &rest args)
-                  (eval `(format ,format-string ,@args))))
-    ad-do-it))
 
 (provide 'init-recentf)
