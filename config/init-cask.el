04ff8334809001f96c250cabf3baabc93e148266
masaaki1001 <masa1001@gmail.com>
2015-04-08 21:54:03 +0900

    auto-install-directoryの指定方法を変更

--- config/init-cask.el
+++ config/init-cask.el
@@ -5,7 +5,7 @@
 
 ;; auto-install
 (require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/auto-install/")
+(setq auto-install-directory (expand-file-name "auto-install" user-emacs-directory))
 (auto-install-compatibility-setup)             ; 互換性確保
 
 (provide 'init-cask)
