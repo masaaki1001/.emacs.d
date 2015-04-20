5a3ed1f2d528ad0db62ce7c13db1c16e485f8dd3
masaaki1001 <masa1001@gmail.com>
2015-04-20 20:31:05 +0900

    一時的にswitch-to-last-buffer-or-other-windowをコメントアウト

--- config/init-buffer.el
+++ config/init-buffer.el
@@ -1,31 +1,31 @@
 ;; バッファ移動を一瞬で行う
 ;; http://d.hatena.ne.jp/rubikitch/20111211/smalldisplay
 ;;; last-buffer
-(defvar last-buffer-saved nil)
-;; last-bufferで選択しないバッファを設定
-(defvar last-buffer-exclude-name-regexp
-  (rx (or "*mplayer*" "*Completions*" "*Org Export/Publishing Help*" "*Messages*" "*Warnings*" "*Packages*" "TAGS"
-          (regexp "^ "))))
-(defun record-last-buffer ()
-  (when (and (one-window-p)
-             (not (eq (window-buffer) (car last-buffer-saved)))
-             (not (string-match last-buffer-exclude-name-regexp
-                                (buffer-name (window-buffer)))))
-    (setq last-buffer-saved
-          (cons (window-buffer) (car last-buffer-saved)))))
-(add-hook 'window-configuration-change-hook 'record-last-buffer)
-(defun switch-to-last-buffer ()
-  (interactive)
-  (condition-case nil
-      (switch-to-buffer (cdr last-buffer-saved))
-    (error (switch-to-buffer (other-buffer)))))
+;; (defvar last-buffer-saved nil)
+;; ;; last-bufferで選択しないバッファを設定
+;; (defvar last-buffer-exclude-name-regexp
+;;   (rx (or "*mplayer*" "*Completions*" "*Org Export/Publishing Help*" "*Messages*" "*Warnings*" "*Packages*" "TAGS"
+;;           (regexp "^ "))))
+;; (defun record-last-buffer ()
+;;   (when (and (one-window-p)
+;;              (not (eq (window-buffer) (car last-buffer-saved)))
+;;              (not (string-match last-buffer-exclude-name-regexp
+;;                                 (buffer-name (window-buffer)))))
+;;     (setq last-buffer-saved
+;;           (cons (window-buffer) (car last-buffer-saved)))))
+;; (add-hook 'window-configuration-change-hook 'record-last-buffer)
+;; (defun switch-to-last-buffer ()
+;;   (interactive)
+;;   (condition-case nil
+;;       (switch-to-buffer (cdr last-buffer-saved))
+;;     (error (switch-to-buffer (other-buffer)))))
 
-(defun switch-to-last-buffer-or-other-window ()
-  (interactive)
-  (if (one-window-p t)
-      (switch-to-last-buffer)
-    (other-window 1)))
-(global-set-key (kbd "C-t") 'switch-to-last-buffer-or-other-window)
+;; (defun switch-to-last-buffer-or-other-window ()
+;;   (interactive)
+;;   (if (one-window-p t)
+;;       (switch-to-last-buffer)
+;;     (other-window 1)))
+;; (global-set-key (kbd "C-t") 'switch-to-last-buffer-or-other-window)
 
 ;; zlc
 (zlc-mode t)
