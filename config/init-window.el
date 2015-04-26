6f7028d5b72a1c6f545ffa8e43379a9361c5e0b7
masaaki1001 <masa1001@gmail.com>
2015-04-26 16:00:25 +0900

    elscreen

--- config/init-window.el
+++ config/init-window.el
@@ -4,10 +4,6 @@
   ;; header-lineの先頭に[<->]を表示しない
   (setq elscreen-tab-display-control nil)
   ;; モードラインにフレーム番号を表示しない
-  (setq elscreen-display-screen-number nil)
-  ;; http://blog.n-z.jp/blog/2013-09-24-elscreen-last-command-char.html
-  (defadvice elscreen-jump (around elscreen-last-command-char-event activate)
-    (let ((last-command-char last-command-event))
-      ad-do-it)))
+  (setq elscreen-display-screen-number nil))
 
 (provide 'init-window)
